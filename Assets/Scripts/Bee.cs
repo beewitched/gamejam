ae06ca98240818ae16766306be23b1f0a747f2ab
bugiflo <bugiflo@gmail.com>
2018-12-08 13:49:03 +0100

    Added bee and bee controles

---
+++ Assets/Scripts/Bee.cs
@@ -0,0 +1,181 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Bee : MonoBehaviour
+{
+    // --- | Serialized | -------------------------------------------------------------------------
+
+    [Header("Settings")]
+    [SerializeField]
+    private float speed = 3f;
+    [SerializeField]
+    private float maxDistanceToPlayer = 1.5f;
+
+    [Header("References")]
+    [SerializeField]
+    private Player player;
+
+
+    // --- | Componetns | -------------------------------------------------------------------------
+
+    private Rigidbody2D controller;
+
+
+    // --- | Variables | --------------------------------------------------------------------------
+
+    private bool isAutoControlled = true;
+    private bool targetPlayer = true;
+
+    private Vector2 targetPos;
+
+    private Vector3[] controllpoints;
+    private int currentControllpointIndex = 0;
+    private Vector3 CurrentControllPoint
+    {
+        get
+        {
+            return controllpoints[currentControllpointIndex];
+        }
+    }
+
+    private System.Action OnBeeArival;
+
+
+    // --- | Methods | ----------------------------------------------------------------------------
+
+    // MonoBehaviour --------------------------------------
+
+    private void Awake()
+    {
+        controller = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (isAutoControlled)
+        {
+            if (targetPlayer)
+            {
+                targetPos = player.transform.position + Vector3.up;
+            }
+            controller.MovePosition(AutoMovement());
+        }
+
+        else
+        {
+            controller.MovePosition(ControlledMovement());
+        }
+    }
+
+    // Public Methods -----------------------------------------------------------------------------
+
+    /// <summary>
+    /// Sends the bee along a path.
+    /// </summary>
+    /// <param name="controllpoints">The path the bee should follow.</param>
+    /// <param name="OnBeeArival">The mehtod called on finishing.</param>
+    public void SendBee(Vector3[] controllpoints, System.Action OnBeeArival)
+    {
+        this.OnBeeArival = OnBeeArival;
+        this.controllpoints = controllpoints;
+        isAutoControlled = false;
+    }
+
+    /// <summary>
+    /// Sets the target position. Bee will idle around it.
+    /// </summary>
+    /// <param name="targetPos">The position to be idleing at.</param>
+    public void SwitchTarget(Vector3 targetPos)
+    {
+        this.targetPos = targetPos;
+        targetPlayer = false;
+    }
+
+    /// <summary>
+    /// Sets the target to the player. Bee will follow the player.
+    /// </summary>
+    public void TargetPlayer()
+    {
+        targetPlayer = true;
+        transform.position = (player.transform.position + Vector3.up);
+    }
+
+    /// <summary>
+    /// Sets the position of the bee to the players.
+    /// </summary>
+    /// <returns>Return the old position of the bee.</returns>
+    public Vector2 PortBeeToPlayer()
+    {
+        Vector2 playerPos = player.transform.position;
+        Vector2 beePos = transform.position;
+        EndControll();
+        if (!targetPlayer)
+        {
+            targetPos = playerPos;
+        }
+        transform.position = playerPos;
+        return beePos;
+    }
+
+    // Private Methods ----------------------------------------------------------------------------
+
+    /// <summary>
+    /// Moves the bee to target position.
+    /// </summary>
+    /// <returns>Returns the new position.</returns>
+    private Vector2 AutoMovement()
+    {
+        Vector2 deltaDistance = targetPos - (Vector2)transform.position;
+        if (deltaDistance.magnitude > maxDistanceToPlayer)
+        {
+            float maxMove = deltaDistance.magnitude / 2;
+            if (maxMove > speed)
+            {
+                maxMove = speed;
+            }
+            return (Vector2)transform.position + deltaDistance.normalized * maxMove * Time.deltaTime;
+        }
+        return transform.position;
+    }
+
+    /// <summary>
+    /// Moves the bee along the path.
+    /// </summary>
+    /// <returns>Returns the new position.</returns>
+    private Vector2 ControlledMovement()
+    {
+        float maxDistance = speed * Time.deltaTime;
+        Vector2 deltaDistance = Vector2.zero;
+        for (int i = currentControllpointIndex; i < controllpoints.Length; i++)
+        {
+            deltaDistance = controllpoints[i] - transform.position;
+            currentControllpointIndex = i;
+            if (deltaDistance.magnitude < maxDistance) { continue; }
+
+
+            return (Vector2)transform.position + deltaDistance.normalized * maxDistance;
+        }
+        return EndControll();
+    }
+
+    /// <summary>
+    /// Stops the bee from following the path.
+    /// </summary>
+    private Vector2 EndControll()
+    {
+        if (!isAutoControlled)
+        {
+            Vector2 beePos = CurrentControllPoint;
+            SwitchTarget(beePos);
+            OnBeeArival();
+            isAutoControlled = true;
+            controllpoints = null;
+            currentControllpointIndex = 0;
+            OnBeeArival = null;
+            return beePos;
+        }
+        return transform.position;
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,22 +7,39 @@ public class Player : MonoBehaviour
 {
     // --- | Serialized | -------------------------------------------------------------------------
 
+    [Header("Settings")]
     [SerializeField]
     private float speed = 2f;
-
     [SerializeField]
     private float jumpStrenght = 3f;
 
+    [Header("Keys")]
+    [SerializeField]
     private KeyCode jumpKey = KeyCode.Space;
+    [SerializeField]
+    private KeyCode switchKey = KeyCode.Alpha3;
 
+    [Header("References")]
+    [SerializeField]
+    Bee bee;
 
-    // --- | Variables | --------------------------------------------------------------------------
+    [SerializeField]
+    private LayerMask obstacleLayers;
+
+    // --- | Components | -------------------------------------------------------------------------
 
     private Rigidbody2D controller;
     private BoxCollider2D collider;
 
 
+    // --- | Variables | --------------------------------------------------------------------------
+
+    private bool canMove = true;
+
+
     // --- | Methods | ----------------------------------------------------------------------------
+
+    // MonoBehaviour --------------------------------------
 
     private void Awake()
     {
@@ -33,30 +50,70 @@ public class Player : MonoBehaviour
 
     private void FixedUpdate()
     {
-        // Get horizontal movement.
-        Vector2 moveDir = controller.velocity;
-        moveDir.x = Input.GetAxisRaw("Horizontal") * speed;
+        if (canMove)
+        {
+            // Get horizontal movement.
+            Vector2 moveDir = controller.velocity;
+            moveDir.x = Input.GetAxisRaw("Horizontal") * speed;
 
-        // Move character.
-        controller.velocity = moveDir;
+            // Move character.
+            controller.velocity = moveDir;
+        }
     }
 
     private void Update()
     {
-        // Detect ground.
-        RaycastHit2D[] leftHits = Physics2D.RaycastAll(transform.position + Vector3.left * collider.bounds.extents.x, Vector2.down);
-        RaycastHit2D[] rightHits = Physics2D.RaycastAll(transform.position + Vector3.right * collider.bounds.extents.x, Vector2.down);
-        bool isGrounded = ((leftHits.Length > 1 && leftHits[1].distance < 0.05f) || (rightHits.Length > 1 && rightHits[1].distance < 0.05f));
-
-        // Jump.
-        if (Input.GetKeyDown(jumpKey) && isGrounded)
+        if (canMove)
         {
-            Jump();
+            // Detect ground.
+            RaycastHit2D[] leftHits = Physics2D.RaycastAll(transform.position + Vector3.left * collider.bounds.extents.x, Vector2.down);
+            RaycastHit2D[] rightHits = Physics2D.RaycastAll(transform.position + Vector3.right * collider.bounds.extents.x, Vector2.down);
+            bool isGrounded = ((leftHits.Length > 1 && leftHits[1].distance < 0.05f) || (rightHits.Length > 1 && rightHits[1].distance < 0.05f));
+
+            // Jump.
+            if (Input.GetKeyDown(jumpKey) && isGrounded)
+            {
+                Jump();
+            }
+
+            // Swich position.
+            if (Input.GetKeyDown(switchKey))
+            {
+                SwitchPos();
+            }
         }
     }
+
+    // Public Methods -------------------------------------
+
+    public void EnableMovement()
+    {
+        canMove = true;
+    }
+
+    public void DisableMovemtn()
+    {
+        canMove = false;
+    }
+
+    // Private Methods ------------------------------------
 
     private void Jump()
     {
         controller.velocity += Vector2.up * jumpStrenght;
+    }
+
+    private void SwitchPos()
+    {
+        if (!CheckSpace(bee.transform.position))
+        {
+            return;
+        }
+        transform.position = bee.PortBeeToPlayer();
+    }
+
+    private bool CheckSpace(Vector2 pos)
+    {
+        return !Physics2D.OverlapBox(pos + collider.offset, collider.bounds.size, 0, obstacleLayers);
     }
 }

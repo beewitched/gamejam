177cb17c7258f81c520475fd6ad8d5a50d70cfc7
bugiflo <bugiflo@gmail.com>
2018-12-08 23:47:58 +0100

    Door stuff

---
+++ Assets/Scripts/Platformer/Interactables/MovingPlatform.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour {
+
+    [SerializeField]
+    private bool loop = false;
+
+    [SerializeField]
+    private Vector2[] controlPoints = new Vector2[2];
+
+
+    private void OnDrawGizmos()
+    {
+        for (int i = 1; i < controlPoints.Length; i++)
+        {
+            Gizmos.DrawLine(controlPoints[i - 1], controlPoints[i]);
+        }
+        if (loop)
+        {
+            Gizmos.DrawLine(controlPoints[controlPoints.Length - 1], controlPoints[0]);
+        }
+    }
+}

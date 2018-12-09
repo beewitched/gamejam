16ba6ee65fc25e5163ce393172aac419c9d2eaf2
Mario Kerndler <mariokerndler@gmail.com>
2018-12-09 20:48:21 +0100

    last scene

---
+++ Assets/Scripts/UI/EndScreen.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndScreen : MonoBehaviour
+{
+
+    public float waitTime = 1f;
+    public Dialogue dialogue;
+    public GameObject textBox;
+    public GameObject dialoguebox;
+    public GameObject dialogManager;
+
+    // Use this for initialization
+    void Start()
+    {
+        textBox.SetActive(false);
+        StartCoroutine(StartDialogue());
+    }
+
+    IEnumerator StartDialogue()
+    {
+        yield return new WaitForSeconds(waitTime);
+        DialogueManager.Instance.StartDialogue(dialogue);
+    }
+
+    public void EndGame()
+    {
+        Application.Quit();
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void StartNext()
+    {
+        dialogManager.SetActive(false);
+        textBox.SetActive(true);
+        dialoguebox.SetActive(false);
+    }
+}

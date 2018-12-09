bcc903a201260857e37a876465d3790bdfb3db2d
bugiflo <bugiflo@gmail.com>
2018-12-09 18:11:06 +0100

    GamaManager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager instance;
+    public static GameManager Instance
+    {
+        get
+        {
+            return instance;
+        }
+    }
+
+    [SerializeField]
+    private PickUp[] cookingList = new PickUp[2];
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+}

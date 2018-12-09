1cb1da0e71c76bd86a124932c3b0f5763ae5f5d3
riskybiznu <davidholy@me.com>
2018-12-09 18:25:25 +0100

    Work on cauldron scene

---
+++ Assets/topfManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class topfManager : MonoBehaviour {
+
+    public Sprite[] ingredients;
+
+	// Use this for initialization
+	void Start () {
+		Inventory.Instance.PickUps
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

0ba6f6ce9c847c80b06d835e9a7ef37741524a7b
riskybiznu <davidholy@me.com>
2018-12-09 12:49:34 +0100

    Level Stuff, level Manager

--- Assets/Scripts/Platformer/Interactables/Beedoor.cs
+++ Assets/Scripts/Platformer/Interactables/Beedoor.cs
@@ -31,19 +31,23 @@ public class Beedoor : MonoBehaviour
     private void OnValidate()
     {
         renderer = GetComponent<SpriteRenderer>();
+        collider = GetComponent<BoxCollider2D>();
 
         if (isOpen)
         {
             renderer.sprite = openSprite;
+            collider.enabled = false;
         }
         else
         {
             renderer.sprite = closedSprite;
+            collider.enabled = true;
         }
     }
 
     private void Awake()
     {
+        renderer = GetComponent<SpriteRenderer>();
         collider = GetComponent<BoxCollider2D>();
     }
 
@@ -53,11 +57,13 @@ public class Beedoor : MonoBehaviour
     {
         isOpen = true;
         renderer.sprite = openSprite;
+        collider.enabled = false;
     }
 
     public void Close()
     {
         isOpen = false;
         renderer.sprite = closedSprite;
+        collider.enabled = true;
     }
 }

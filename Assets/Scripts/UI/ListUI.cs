4e6c1c6089473a935d33e94dec041a3aeaad5280
Mario Kerndler <mariokerndler@gmail.com>
2018-12-09 18:35:54 +0100

    list addition2

---
+++ Assets/Scripts/UI/ListUI.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ListUI : MonoBehaviour {
+
+    public Sprite[] mushroomSprite = new Sprite[3];
+    public Sprite[] eggSprite = new Sprite[3];
+    public Sprite[] flowerSprite = new Sprite[3];
+    public GameObject ingredientDisplay;
+
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
